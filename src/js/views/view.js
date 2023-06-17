679fa955954735947d0709beccaef6242186d318
hongfeij <hongfeij@andrew.cmu.edu>
2023-06-17 23:35:45 +0800

    recreate files for netlify 02

---
+++ src/js/views/view.js
@@ -0,0 +1,102 @@
+import icons from 'url:../../img/icons.svg';
+
+export default class View {
+  _data;
+
+  /**
+   * Render the recevied object to the DOM
+   * @param {Object | Object[]} data The data to be rendered (e.g. recipe)
+   * @param {boolean} [render = true] If false, create markup string instead of rendering to the DOM
+   * @returns {undefined | string} A markup is returned if render=false
+   */
+  render(data, render = true) {
+    if (!data || (Array.isArray(data) && data.length === 0))
+      return this.renderError();
+    this._data = data;
+    const markUp = this._generateMarkup();
+
+    if (!render) {
+      return markUp;
+    }
+
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markUp);
+  }
+
+  update(data) {
+    this._data = data;
+    const newMarkup = this._generateMarkup();
+    const newDOM = document.createRange().createContextualFragment(newMarkup);
+    const newElements = Array.from(newDOM.querySelectorAll('*'));
+    const currElements = Array.from(this._parentElement.querySelectorAll('*'));
+
+    newElements.forEach((newEl, i) => {
+      const currEl = currElements[i];
+
+      // Update changed TEXT
+      if (
+        !newEl.isEqualNode(currEl) &&
+        newEl.firstChild?.nodeValue.trim() !== ''
+      ) {
+        currEl.textContent = newEl.textContent;
+      }
+
+      // Update changed ATTRIBUTES
+      if (!newEl.isEqualNode(currEl)) {
+        Array.from(newEl.attributes).forEach(attr =>
+          currEl.setAttribute(attr.name, attr.value)
+        );
+      }
+    });
+  }
+
+  _clear() {
+    this._parentElement.innerHTML = '';
+  }
+
+  renderSpinner() {
+    const prevHTML = this._parentElement.innerHTML;
+    const markUp = `
+        <div class="spinner">
+          <svg>
+            <use href="${icons}.svg#icon-loader"></use>
+          </svg>
+        </div>
+      `;
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markUp);
+    return prevHTML;
+  }
+
+  renderError(message = this._errorMessage) {
+    const markUp = `
+        <div class="error">
+          <div>
+            <svg>
+              <use href="${icons}#icon-alert-triangle"></use>
+            </svg>
+          </div>
+          <p>${message}</p>
+        </div>
+      `;
+
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markUp);
+  }
+
+  renderMessage(message = this._message) {
+    const markUp = `
+        <div class="message">
+          <div>
+            <svg>
+              <use href="${icons}#icon-smile"></use>
+            </svg>
+          </div>
+          <p>${message}</p>
+        </div>
+      `;
+
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markUp);
+  }
+}
